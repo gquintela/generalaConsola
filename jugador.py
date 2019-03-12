c607f8f8b0d70eedcd166316dec8eedee224a9ca
Gonzalo Andres Quintela <gquintela@dc.uba.ar>
2019-03-12 19:26:48 -0300

    trabajando impresion puntajes

--- jugador.py
+++ jugador.py
@@ -1,7 +1,19 @@
-from categorias import *
+
 
 class Jugador:
     _nombre=""
     _puntajeTotal=0
     _numeroDeTirada=0
-    _categorias= Categorias()
+    _categorias= [
+        ["UNO",3,"tachado"],
+        ["DOS",0,"vacggio"],
+        ["TRES",0,"vacio"],
+        ["CUATRO",0,"vacio"],
+        ["CINCO",0,"vacio"],
+        ["SEIS",0,"vacio"],
+        ["ESCALERA",0,"vacio"],
+        ["FULL",0,"vacio"],
+        ["POKER",0,"vacio"],
+        ["GENERALA",0,"vacio"],
+        ["GENERALA DOBLE",0,"vacio"]
+    ]
--- main.py
+++ main.py
@@ -2,28 +2,49 @@ from generala import *
 from jugador import *
 from tablaPuntaje import *
 
+def imprimirPuntos(jugador, cat):
+    if (jugador._categorias[cat][2]=="vacio"):       #como puedo hacer para que quede mas declarativo?
+        print ("     ")
+    elif (jugador._categorias[cat][2]=="tachado"):
+        print ("   xXx   ")
+    else:
+        print("    ",end="")
+        print (jugador._categorias[cat][1])
+
 
 def imprimirTablaPuntaje (jugador):
     print( jugador._nombre )
-    print("       UNO:    ")
-    print("       DOS:    ")
-    print("       TRES:   ")
-    print("      CUATRO:  ")
-    print("      CINCO:   ")
-    print("      SEIS:    ")
-    print("     ESCALERA: ")
-    print("      FULL:    ")
-    print("     POKER:    ")
-    print("    GENERALA:  ")
-    print("GENERALA DOBLE:")
+    print("       UNO:    ",end="")
+    imprimirPuntos(jugador,0)
+    print("       DOS:    ",end="")
+    imprimirPuntos(jugador,1)
+    print("       TRES:   ",end="")
+    imprimirPuntos(jugador,2)
+    print("      CUATRO:  ",end="")
+    imprimirPuntos(jugador,3)
+    print("      CINCO:   ",end="")
+    imprimirPuntos(jugador,4)
+    print("      SEIS:    ",end="")
+    imprimirPuntos(jugador,5)
+    print("     ESCALERA: ",end="")
+    imprimirPuntos(jugador,6)
+    print("      FULL:    ",end="")
+    imprimirPuntos(jugador,7)
+    print("     POKER:    ",end="")
+    imprimirPuntos(jugador,8)
+    print("    GENERALA:  ",end="")
+    imprimirPuntos(jugador,9)
+    print("GENERALA DOBLE:",end="")
+    imprimirPuntos(jugador,10)
+    print ("-----------------")
+    print("PUNTAJE TOTAL: ",end="")
+    print (jugador._puntajeTotal)
 
-    if (jugador._categorias._uno[1]=="vacio"):
-        print ("     ")
-    elif (jugador._categorias._uno[1]=="tachado"):
-        print ("   xXx   ")
-    else:
-        print (jugador._categorias._uno[0])
 
+
+
+###############################################################
+###############################################################
 
 print ("Generala para una persona\n\n")
 
