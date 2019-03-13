91507b65b8e95729a87c7fc5f5b2fafb2dd744b4
gquintela <gquintela@users.noreply.github.com>
2019-03-13 11:01:52 -0300

    Create cubilete.py

---
+++ cubilete.py
@@ -0,0 +1,21 @@
+from random import randint
+
+class Cubilete:
+  _dado1=0
+  _dado2=0
+  _dado3=0
+  _dado4=0
+  _dado5=0
+
+  def tirarCubilete(self):
+    pass
+
+  def mostrarValores(self):
+    return self._valor
+  
+  def imprimirCubilete(self):
+    print ("Dado 1: %s" % self._dado1 )
+    print ("Dado 2: %s" % self._dado2 )
+    print ("Dado 3: %s" % self._dado3 )
+    print ("Dado 4: %s" % self._dado4 )
+    print ("Dado 5: %s" % self._dado5 )
